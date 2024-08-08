7debca9bd7cd9333391ce8b3d44bde11e29174ea
Katie795 <35525848+Katie795@users.noreply.github.com>
2024-08-08 09:31:30 +0100

    Update mwe.py so Encoder outputs Tensors of the correct shape
    This update means that A = J for the Encoder and both are nonzero

--- src/mwe.py
+++ src/mwe.py
@@ -9,36 +9,38 @@ from pyadjoint import ReducedFunctional, Control
 from firedrake.ml.pytorch import fem_operator
 from pyadjoint.tape import set_working_tape
 
-# Construct mesh and point mesh onto which we want to interpolate
-mesh = UnitSquareMesh(4, 4)
+
+# Construct meshes onto which we want to interpolate
+mesh = UnitSquareMesh(3, 3)
+
 points = [[0.6, 0.1], [0.5, 0.4], [0.7, 0.9]]
 vom = VertexOnlyMesh(mesh, points)
 
 # Function spaces on these meshes
 fs = FunctionSpace(mesh, "CG", 1)
-vertex_only_fs = FunctionSpace(vom, "DG", 0)
+fs2 = FunctionSpace(vom, "DG", 0)
 
 # Sizes of function spaces
-n_in = int(fs.dof_count)
-n_out = int(vertex_only_fs.dof_count)
-
+n_in = len(Function(fs).dat.data)
+n_out = len(Function(fs2).dat.data)
 
 class Encoder(torch.nn.Module):
-    """Differentiable encoder which maps a function from a function space to the points"""
+    """Differentiable encoder which maps a function from a function space to
+    another function space"""
 
-    def __init__(self, fs, vertex_only_fs):
+    def __init__(self, fs, fs2):
         """Initialise new instance
 
         :arg fs: original function space
-        :arg vertex_only_fs: vertex-only function space to which we want to interpolate
+        :arg fs2: vom function space to which we want to interpolate
         """
         super().__init__()
         self.in_features = int(fs.dof_count)
-        self.out_features = int(vertex_only_fs.dof_count)
+        self.out_features = int(fs2.dof_count)
         continue_annotation()
         with set_working_tape() as _:
             u = Function(fs)
-            interpolator = interpolate(TestFunction(fs), vertex_only_fs)
+            interpolator = interpolate(TestFunction(fs), fs2)
             self._function_to_patch = fem_operator(
                 ReducedFunctional(assemble(action(interpolator, u)), Control(u))
             )
@@ -53,30 +55,32 @@ class Encoder(torch.nn.Module):
         :arg x: input tensor
         """
         # apply differentiable interpolation to each tensor in the batch
-        if len(x.shape) == 1:
-            return torch.flatten(self._function_to_patch(x))
+        output_tensor = self._function_to_patch(torch.unbind(x)[0]).squeeze()
+        if x.shape[0] == 1:
+            pass
         else:
-            return torch.stack(
-                [torch.flatten(self._function_to_patch(y)) for y in torch.unbind(x)]
-            )
+            for i in range(1, x.shape[0]): # the number of batches we are doing
+                batch_data = self._function_to_patch(torch.unbind(x)[i])
+                output_tensor = torch.vstack([output_tensor, batch_data])
+        return output_tensor
 
 
 class Decoder(torch.nn.Module):
     """Differentiable decoder which maps a function from the points to a function space"""
 
-    def __init__(self, fs, vertex_only_fs):
+    def __init__(self, fs, fs2):
         """Initialise new instance
 
         :arg fs: original function space
-        :arg vertex_only_fs: vertex-only function space to which we want to interpolate
+        :arg fs2:function space to which we want to interpolate
         """
         super().__init__()
-        self.in_features = int(vertex_only_fs.dof_count)
+        self.in_features = int(fs2.dof_count)
         self.out_features = int(fs.dof_count)
         continue_annotation()
         with set_working_tape() as _:
-            w = Cofunction(vertex_only_fs.dual())
-            interpolator = interpolate(TestFunction(fs), vertex_only_fs)
+            w = Cofunction(fs2.dual())
+            interpolator = interpolate(TestFunction(fs), fs2)
             self._patch_to_function = fem_operator(
                 ReducedFunctional(
                     assemble(action(adjoint(interpolator), w)), Control(w)
@@ -93,20 +97,17 @@ class Decoder(torch.nn.Module):
         :arg x: input tensor
         """
         # apply differentiable interpolation to each tensor in the batch
-        if len(x.shape) == 1:
-            return torch.flatten(self._patch_to_function(x))
-        else:
-            return torch.stack(
-                [torch.flatten(self._patch_to_function(y)) for y in torch.unbind(x)]
-            )
+        return torch.stack([self._patch_to_function(y) for y in torch.unbind(x)])
 
 
 # PyTorch model: linear layer + encoder layer
 
+# model(theta) = f ( g (theta) )
+# dmodel/dtheta
 model = torch.nn.Sequential(
     torch.nn.Linear(in_features=n_in, out_features=n_in).double(),
-    Encoder(fs, vertex_only_fs).double(),
-    Decoder(fs, vertex_only_fs).double(),
+    Encoder(fs, fs2).double(),
+    Decoder(fs, fs2).double(),
 )
 
 # Input and target tensors (random)
@@ -138,31 +139,35 @@ y = model(X)
 loss = loss_fn(y, y_target)
 loss.backward()
 
-
 for layer in (
     torch.nn.Linear(in_features=3, out_features=7, bias=False).double(),
-    Encoder(fs, vertex_only_fs).double(),
-    Decoder(fs, vertex_only_fs).double(),
+    Encoder(fs, fs2).double(),
+    Decoder(fs, fs2).double(),
 ):
     n_in = layer.in_features
     n_out = layer.out_features
+
     # extract matrix
     A = np.zeros((n_out, n_in))
     for j in range(n_in):
-        x = torch.zeros(n_in, dtype=torch.float64)
-        x[j] = 1.0
-        y = layer(x)
-        z = y.detach()
+        x = torch.zeros((1, n_in), dtype=torch.float64) 
+        x[0, j] = 1.0 
+        x.unsqueeze(dim=0)
+        y = layer(x) 
         A[:, j] = np.asarray(y.detach())
     x = torch.zeros(n_in, dtype=torch.float64)
+    x.unsqueeze(dim=0)
     # extract Jacobian
-    J = np.asarray(torch.autograd.functional.jacobian(layer, x))
+    J = np.asarray(torch.autograd.functional.jacobian(layer, x.unsqueeze(dim=0))).squeeze()
     print("layer = ", str(layer))
+    print(f"A is {type(A)}")
+    print(f"Shape of A is {A.shape}")
     print("||A|| :")
-    print(A.shape, np.linalg.norm(A))
+    print(np.linalg.norm(A))
     print()
+    print(f"Shape of J is {J.shape}")
     print("||J|| :")
-    print(J.shape, np.linalg.norm(J))
+    print(np.linalg.norm(J))
     print()
     print("difference ||A-J|| :")
     print(np.linalg.norm(A - J))
