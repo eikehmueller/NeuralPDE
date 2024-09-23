1865da21e50a93d1ab41530d3d4610e57d705dde
Eike Mueller <eike.h.mueller@gmail.com>
2024-09-23 13:18:53 +0100

    Removed old code

--- src/intergrid.py
+++ src/intergrid.py
@@ -40,89 +40,6 @@ def torch_interpolation_tensor(fs_from, fs_to, transpose=False):
     return a.to_sparse()
 
 
-class InterpolatorWrapper(torch.autograd.Function):
-    """Differentiable PyTorch wrapper around Firedrake interpolation
-
-    Allows interpolation of functions between two Firedrake function
-    spaces. Both the forward and adjoint operation are implemented. Since
-    interpolation is a linear operation y = A.x, the gradient is propagated
-    like grad_in = A^T.grad_out
-    """
-
-    @staticmethod
-    def interpolate(metadata, x):
-        """Interpolate a function given by its dof-vector
-
-        :arg metadata: information on function spaces
-        :arg x: dof-vector of function on input function space
-        """
-        u = from_torch(x, V=metadata["fs_from"])
-        w = assemble(action(metadata["interpolator"], u))
-        return torch.flatten(to_torch(w))
-
-    @staticmethod
-    def adjoint_interpolate(metadata, x):
-        """Map a dual vector on the output function space to the input space
-
-        :arg metadata: information on function spaces
-        :arg x: dof-vector of dual function on output function space
-        """
-        w = from_torch(x, V=metadata["fs_to"].dual())
-        u = assemble(action(adjoint(metadata["interpolator"]), w))
-        return torch.flatten(to_torch(u))
-
-    @staticmethod
-    def forward(ctx, metadata, x):
-        """Forward map
-
-        Computes y = A.x if x is a dof-vector on the input function space
-        or y = A^T.x if x is a dual vector on the output function space
-
-        :arg ctx: context
-        :arg metadata: information on function spaces
-        :arg x: tensor to operate on
-        """
-        ctx.metadata.update(metadata)
-        if metadata["reverse"]:
-            return InterpolatorWrapper.adjoint_interpolate(metadata, x)
-        else:
-            return InterpolatorWrapper.interpolate(metadata, x)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        """Backward map
-
-        :arg ctx: context
-        :arg grad_output: gradient with respect out output variables
-        """
-        if ctx.metadata["reverse"]:
-            return None, InterpolatorWrapper.interpolate(ctx.metadata, grad_output)
-        else:
-            return None, InterpolatorWrapper.adjoint_interpolate(
-                ctx.metadata, grad_output
-            )
-
-
-def interpolator_wrapper(fs_from, fs_to, reverse=False):
-    """Construct interpolation object from
-
-    Depending on the value of reverse, this either interpolates the
-    dof-vectors of functions from fs_from to fs_to or the dof-vectors of
-    dual functions from fs_to to fs_from
-
-    :arg fs_from: function space which we want to interpolate from
-    :arg fs_to: function space which we want to interpolate to
-    """
-    interpolator = interpolate(TestFunction(fs_from), fs_to)
-    metadata = {
-        "fs_from": fs_from,
-        "fs_to": fs_to,
-        "interpolator": interpolator,
-        "reverse": reverse,
-    }
-    return partial(InterpolatorWrapper.apply, metadata)
-
-
 class Encoder(torch.nn.Module):
     """Differentiable encoder which interpolates to a different function space
 
@@ -132,7 +49,7 @@ class Encoder(torch.nn.Module):
     v = interpolate(u).
     """
 
-    def __init__(self, fs_from, fs_to, assemble=True):
+    def __init__(self, fs_from, fs_to):
         """Initialise new instance
 
         :arg fs_from: original function space
@@ -142,13 +59,9 @@ class Encoder(torch.nn.Module):
         super().__init__()
         self.in_features = int(fs_from.dof_count)
         self.out_features = int(fs_to.dof_count)
-        self.assemble = assemble
-        if self.assemble:
-            self.register_buffer(
-                "a_sparse", torch_interpolation_tensor(fs_from, fs_to, transpose=True)
-            )
-        else:
-            self._interpolate = interpolator_wrapper(fs_from, fs_to, reverse=False)
+        self.register_buffer(
+            "a_sparse", torch_interpolation_tensor(fs_from, fs_to, transpose=True)
+        )
 
     def forward(self, x):
         """Forward pass
@@ -158,21 +71,7 @@ class Encoder(torch.nn.Module):
 
         :arg x: input tensor
         """
-        if self.assemble:
-            return torch.matmul(x, self.a_sparse)
-        else:
-            return self._forward(x)
-
-    def _forward(self, x):
-        """Recursively apply forward pass
-
-        Unbind the input, recursively call the function and stack the outputs.
-        If x has dimension 1, apply the interpolation.
-        """
-        if x.dim() == 1:
-            return self._interpolate(x)
-        else:
-            return torch.stack([self._forward(y) for y in torch.unbind(x)])
+        return torch.matmul(x, self.a_sparse)
 
 
 class Decoder(torch.nn.Module):
@@ -184,25 +83,18 @@ class Decoder(torch.nn.Module):
     adjoint of the Encoder operation.
     """
 
-    def __init__(self, fs_from, fs_to, assemble=True):
+    def __init__(self, fs_from, fs_to):
         """Initialise new instance
 
         :arg fs_from: original function space that we want to interpolate from
         :arg fs_to: target function space to which we want to interpolate
-        :arg assemble: assemble sparse torch matrix of interpolation
         """
         super().__init__()
         self.in_features = int(fs_to.dof_count)
         self.out_features = int(fs_from.dof_count)
-        self.assemble = assemble
-        if self.assemble:
-            self.register_buffer(
-                "a_sparse", torch_interpolation_tensor(fs_from, fs_to, transpose=False)
-            )
-        else:
-            self._adjoint_interpolate = interpolator_wrapper(
-                fs_from, fs_to, reverse=True
-            )
+        self.register_buffer(
+            "a_sparse", torch_interpolation_tensor(fs_from, fs_to, transpose=False)
+        )
 
     def forward(self, x):
         """Forward pass.
@@ -212,18 +104,4 @@ class Decoder(torch.nn.Module):
 
         :arg x: input tensor
         """
-        if self.assemble:
-            return torch.matmul(x, self.a_sparse)
-        else:
-            return self._forward(x)
-
-    def _forward(self, x):
-        """Recursively apply forward pass
-
-        Unbind the input, recursively call the function and stack the outputs.
-        If x has dimension 1, apply the revserve interpolation.
-        """
-        if x.dim() == 1:
-            return self._adjoint_interpolate(x)
-        else:
-            return torch.stack([self._forward(y) for y in torch.unbind(x)])
+        return torch.matmul(x, self.a_sparse)
