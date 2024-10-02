2a0dc97a722999a8db85cb979dafd77695f03e47
Katie795 <katherinemackenzie795@gmail.com>
2024-10-02 16:37:43 +0100

    fixing train_imi.sh

--- src/train_imi.sh
+++ src/train_imi.sh
@@ -1,3 +1,4 @@
+#source /opt/firedrake/firedrake/bin/activate
 mkdir -p "/u/h/ah3557/katie/NeuralPDE/output";
 
-python train.py --path_to_output_folder=/home/katie795/internship/NeuralPDE/output
+python train.py --path_to_output_folder=/u/h/ah3557/katie/NeuralPDE/output
