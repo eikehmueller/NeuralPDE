d3b902707fa8390d07f14e010f8b80bbaa459f09
Katie795 <35525848+Katie795@users.noreply.github.com>
2024-10-07 11:42:02 +0100

    Update data_producer.py

--- src/data_producer.py
+++ src/data_producer.py
@@ -12,14 +12,18 @@ if not os.path.exists("data"):
 
 phi = 1                  # rotation angle of the data
 degree = 4               # degree of the polynomials on the dataset
+batchsize = 1024         # batchsize
+n_ref = 2                # number of refinements in the icosahedral mesh
+valid_batchsize = 64     # batchsize for validation group
 
-mesh = UnitIcosahedralSphereMesh(2) # create the mesh
+mesh = UnitIcosahedralSphereMesh(n_ref) # create the mesh
 V = FunctionSpace(mesh, "CG", 1) # define the function space
 
-train_ds = AdvectionDataset(V, 512, phi, degree, seed=12345)
+train_ds = AdvectionDataset(V, batchsize, phi, degree, seed=1234)
 train_ds.generate()
-train_ds.save("data/data_512_2_1.npy")
+train_ds.save(f"data/data_{batchsize}_{n_ref}_{phi}.npy")
 
 valid_ds = AdvectionDataset(V, 32, phi, degree, seed=123)  
 valid_ds.generate()
-valid_ds.save("data/data_32_2_1.npy")
+valid_ds.save(f"data/data_{valid_batchsize}_{n_ref}_{phi}.npy")
+
