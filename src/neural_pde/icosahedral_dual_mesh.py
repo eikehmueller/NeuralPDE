cffdaea2b437f6a63439426d17bd9b30ccb617e9
Eike Mueller <em459@bath.ac.uk>
2025-09-26 15:31:25 +0100

    Added method for projecting to DG function space of original mesh for visualisation

--- src/neural_pde/icosahedral_dual_mesh.py
+++ src/neural_pde/icosahedral_dual_mesh.py
@@ -11,8 +11,8 @@ class IcosahedralDualMesh:
 
     def __init__(self, nref):
         self.nref = nref
-        mesh = UnitIcosahedralSphereMesh(nref)
-        plex = mesh.topology_dm
+        self._mesh = UnitIcosahedralSphereMesh(nref)
+        plex = self._mesh.topology_dm
         dim = plex.getCoordinateDim()
         vertex_coords = np.asarray(plex.getCoordinates()).reshape([-1, dim])
         pCellStart, pCellEnd = plex.getHeightStratum(0)
@@ -51,3 +51,21 @@ class IcosahedralDualMesh:
     def neighbour_list(self):
         """return neighbour list"""
         return list(self._neighbours)
+
+    def project_to_dg0(self, u_dual):
+        """Project data from dual mesh to DG0 space on original mesh
+
+        :arg u_dual: vector of length n, where n is the number of vertices of the dual mesh,
+                     whcih equals the number of cells of the original mesh
+
+        returns a DG0 function on the original mesh
+        """
+        fs = FunctionSpace(self._mesh, "DG", 0)
+        u = Function(fs)
+        plex = self._mesh.topology_dm
+        section = fs.dm.getDefaultSection()
+        pCellStart, pCellEnd = plex.getHeightStratum(0)
+        for cell in range(pCellStart, pCellEnd):
+            offset = section.getOffset(cell)
+            u.dat.data[offset] = u_dual[cell - pCellStart]
+        return u
