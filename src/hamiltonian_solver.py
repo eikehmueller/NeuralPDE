ffbc078a747ad52c65b4ba105e74965e71cb4d9d
Eike Mueller <eike.h.mueller@gmail.com>
2025-11-06 14:09:21 +0000

    Some refactoring and tidying up

--- src/hamiltonian_solver.py
+++ src/hamiltonian_solver.py
@@ -5,33 +5,59 @@ import torch
 class Solver(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, input, F_q, F_p, n, dt):
-        m = input.shape[-1]
-        q, p = torch.split(input.clone().detach(), m // 2, dim=-1)
+    def forward(ctx, x, F_q, F_p, d_lat, d_ancil, n_t, dt):
+        """Forward pass, compute state at time T = n_t*dt
+
+        The input x in R^{d_lat+d_ancil} is split as x = (q_0,p_0,xi) where q, p are vectors
+        of length d_lat/2 and xi is a vector of length d_ancil. The function returns (q_T,p_T,xi)
+        where q_T, p_T are obtained by integrating to time T = n_t*dt with the symplectic integrator.
+        The functions F_q and F_p are of the form
+
+            F_q, F_p: R^{d_lat/2} x R^{d_ancil} -> R^{d_lat/2}
+
+        and they each depend on a set of parameters theta_q, theta_p.
+
+        :arg x: input state of shape (B,d_{lat}+d_{ancil})
+        :arg F_q: forcing function for q-part of state vector
+        :arg F_p: forcing function for p-part of state vector
+        :arg d_lat: dimension of dynamic latent space
+        :arg d_ancil: dimension of ancillary space
+        """
+        assert (
+            x.shape[-1] == d_lat + d_ancil
+        ), "Last dimension must be of size d_lat + d_ancil"
+        assert d_lat == 2 * d_lat // 2, "Dimension d_lat as to be a multiple of two"
+        q, p, xi = torch.split(
+            x.clone().detach(), [d_lat // 2, d_lat // 2, d_ancil], dim=-1
+        )
         ctx.F_q = F_q
         ctx.F_p = F_p
-        ctx.n = n
+        ctx.d_lat = d_lat
+        ctx.d_ancil = d_ancil
+        ctx.n_t = n_t
         ctx.dt = dt
         with torch.no_grad():
-            q = q + dt / 2 * F_q(p)
-            for j in range(n):
-                p = p + dt * F_p(q)
-                rho = 1 / 2 if j == n - 1 else 1
-                q = q + rho * dt * F_q(p)
-            output = torch.cat([q, p], dim=-1)
+            q = q + dt / 2 * F_q(p, xi)
+            for j in range(n_t):
+                p = p + dt * F_p(q, xi)
+                rho = 1 / 2 if j == n_t - 1 else 1
+                q = q + rho * dt * F_q(p, xi)
+            output = torch.cat([q, p, xi], dim=-1)
             ctx.save_for_backward(output)
         return output
 
     @staticmethod
-    def _backward_step(z_1, z_2, F, grad_1, grad_2, theta_2, scaling_factor):
+    def _backward_step(
+        z_1, z_2, xi, F, grad_1, grad_2, grad_xi, theta_2, scaling_factor
+    ):
         with torch.no_grad():
-            z_1 -= scaling_factor * F(z_2)
+            z_1 -= scaling_factor * F(z_2, xi)
         _z_2 = z_2.detach()
         _z_2.requires_grad = True
         with torch.enable_grad():
-            dz_1 = F(_z_2)
-        (dF_2, *dtheta_2) = torch.autograd.grad(
-            dz_1, [_z_2] + theta_2, grad_outputs=grad_2
+            dz_1 = F(_z_2, xi)
+        (dF_2, dF_xi, *dtheta_2) = torch.autograd.grad(
+            dz_1, [_z_2, xi] + theta_2, grad_outputs=grad_2
         )
         for x, y in zip(theta_2, dtheta_2):
             if x.grad is None:
@@ -39,19 +65,29 @@ class Solver(torch.autograd.Function):
             else:
                 x.grad += scaling_factor * y
         grad_1 += scaling_factor * dF_2
+        grad_xi += scaling_factor * dF_xi
 
     @staticmethod
     def backward(ctx, grad_output):
         (output,) = ctx.saved_tensors
-        m = output.shape[-1]
-        q, p = torch.split(output.clone().detach(), m // 2, dim=-1)
-        grad_q, grad_p = torch.split(grad_output.clone().detach(), m // 2, dim=-1)
+        split = [ctx.d_lat // 2, ctx.d_lat // 2, ctx.d_ancil]
+        q, p, xi = torch.split(output.clone().detach(), split, dim=-1)
+        xi.requires_grad = True
+        grad_q, grad_p, grad_xi = torch.split(
+            grad_output.clone().detach(), split, dim=-1
+        )
         theta_q = list(ctx.F_q.parameters())
         theta_p = list(ctx.F_p.parameters())
-        for j in range(ctx.n):
+        for j in range(ctx.n_t):
             rho = 1 / 2 if j == 0 else 1
-            Solver._backward_step(q, p, ctx.F_q, grad_p, grad_q, theta_q, rho * ctx.dt)
-            Solver._backward_step(p, q, ctx.F_p, grad_q, grad_p, theta_p, ctx.dt)
-        Solver._backward_step(q, p, ctx.F_q, grad_p, grad_q, theta_q, ctx.dt / 2)
-        grad_input = torch.cat([grad_q, grad_p], dim=-1)
-        return grad_input, None, None, None, None
+            Solver._backward_step(
+                q, p, xi, ctx.F_q, grad_p, grad_q, grad_xi, theta_q, rho * ctx.dt
+            )
+            Solver._backward_step(
+                p, q, xi, ctx.F_p, grad_q, grad_p, grad_xi, theta_p, ctx.dt
+            )
+        Solver._backward_step(
+            q, p, xi, ctx.F_q, grad_p, grad_q, grad_xi, theta_q, ctx.dt / 2
+        )
+        grad_input = torch.cat([grad_q, grad_p, grad_xi], dim=-1)
+        return grad_input, None, None, None, None, None, None
--- src/test_hamiltonian_solver.py
+++ src/test_hamiltonian_solver.py
@@ -1,80 +1,101 @@
 import numpy as np
 import torch
 from hamiltonian_solver import Solver
-import pytest
+
+
+class HamiltonianForcing(torch.nn.Module):
+    def __init__(self, d_dyn, d_ancil):
+        super().__init__()
+        self.linear = torch.nn.Linear(d_dyn + d_ancil, d_dyn, bias=True)
+        self.d_dyn = d_dyn
+        self.d_ancil = d_ancil
+
+    def forward(self, z, xi):
+        x = torch.cat((z, xi), dim=-1)
+        return self.linear(x)
+
+
+def models(d_lat, d_ancil):
+    F_q = HamiltonianForcing(d_lat // 2, d_ancil)
+    F_p = HamiltonianForcing(d_lat // 2, d_ancil)
+    return F_q, F_p
 
 
 def autograd_solver(model_q, model_p, X, n, dt):
+    d_lat = 2 * model_q.d_dyn
+    d_ancil = model_q.d_ancil
     model_q.zero_grad()
     model_p.zero_grad()
     F = Solver.apply
-    Y = F(X, model_q, model_p, n, dt)
+    Y = F(X, model_q, model_p, d_lat, d_ancil, n, dt)
     loss = torch.sum(Y**2)
     loss.backward()
     return loss
 
 
 def naive_solver(model_q, model_p, X, n, dt):
+    d_lat = 2 * model_q.d_dyn
+    d_ancil = model_q.d_ancil
     model_q.zero_grad()
     model_p.zero_grad()
     m = X.shape[-1]
-    q0, p0 = torch.split(X.clone().detach(), m // 2, dim=-1)
+    q0, p0, xi = torch.split(
+        X.clone().detach(), [d_lat // 2, d_lat // 2, d_ancil], dim=-1
+    )
     q0.requires_grad = True
     p0.requires_grad = True
+    xi.requires_grad = True
     q = q0
     p = p0
-    q = q + dt / 2 * model_q(p)
+    q = q + dt / 2 * model_q(p, xi)
     for j in range(n):
-        p = p + dt * model_p(q)
+        p = p + dt * model_p(q, xi)
         rho = 1 / 2 if j == n - 1 else 1
-        q = q + rho * dt * model_q(p)
-    loss = torch.sum(q**2) + torch.sum(p**2)
+        q = q + rho * dt * model_q(p, xi)
+    loss = torch.sum(q**2) + torch.sum(p**2) + torch.sum(xi**2)
     loss.backward()
     return loss
 
 
-def models(m):
-    linear_q = torch.nn.Linear(m // 2, m // 2, bias=True)
-    linear_p = torch.nn.Linear(m // 2, m // 2, bias=True)
-    return linear_q, linear_p
-
-
 def test_hamiltonian_loss():
     dt = 0.1
-    n = 8
-    m = 8
-    model_q, model_p = models(m)
-    X = torch.tensor(np.arange(m, dtype=np.float32), requires_grad=True)
-    loss_autograd = autograd_solver(model_q, model_p, X, n, dt)
-    loss_naive = naive_solver(model_q, model_p, X, n, dt)
+    n_t = 8
+    d_lat = 8
+    d_ancil = 3
+    model_q, model_p = models(d_lat, d_ancil)
+    X = torch.tensor(np.arange(d_lat + d_ancil, dtype=np.float32), requires_grad=True)
+    loss_autograd = autograd_solver(model_q, model_p, X, n_t, dt)
+    loss_naive = naive_solver(model_q, model_p, X, n_t, dt)
     assert np.allclose(loss_autograd.detach(), loss_naive.detach())
 
 
 def test_hamiltonian_input_gradients():
     dt = 0.1
-    n = 8
-    m = 8
-    model_q, model_p = models(m)
-    X = torch.tensor(np.arange(m, dtype=np.float32), requires_grad=True)
-    autograd_solver(model_q, model_p, X, n, dt)
+    n_t = 8
+    d_lat = 8
+    d_ancil = 3
+    model_q, model_p = models(d_lat, d_ancil)
+    X = torch.tensor(np.arange(d_lat + d_ancil, dtype=np.float32), requires_grad=True)
+    autograd_solver(model_q, model_p, X, n_t, dt)
     grad_autograd = X.grad
-    naive_solver(model_q, model_p, X, n, dt)
+    naive_solver(model_q, model_p, X, n_t, dt)
     grad_naive = X.grad
     assert np.allclose(grad_autograd, grad_naive)
 
 
 def test_hamiltonian_parameter_gradients():
     dt = 0.1
-    n = 8
-    m = 8
-    model_q, model_p = models(m)
-    X = torch.tensor(np.arange(m, dtype=np.float32), requires_grad=True)
-    autograd_solver(model_q, model_p, X, n, dt)
+    n_t = 8
+    d_lat = 8
+    d_ancil = 3
+    model_q, model_p = models(d_lat, d_ancil)
+    X = torch.tensor(np.arange(d_lat + d_ancil, dtype=np.float32), requires_grad=True)
+    autograd_solver(model_q, model_p, X, n_t, dt)
     grad_autograd = []
     for p in list(model_q.parameters()) + list(model_p.parameters()):
         grad_autograd.append(p.grad.detach())
 
-    naive_solver(model_q, model_p, X, n, dt)
+    naive_solver(model_q, model_p, X, n_t, dt)
     grad_naive = []
     for p in list(model_q.parameters()) + list(model_p.parameters()):
         grad_naive.append(p.grad.detach())
