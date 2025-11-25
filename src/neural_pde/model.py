fixed the normalisation

src/neural_pde/model.py
@@ -312,7 +312,7 @@ class NeuralPDEModel(torch.nn.Module):
         """
         x_mean = self.x_mean.to(x.device)
         x_std = self.x_std.to(x.device)
-        x_normalised = x
+        x_normalised = x.detach().clone()
         x_normalised[:, 0: self.n_func_in_dynamic, :] = (x[:, 0: self.n_func_in_dynamic, :] - x_mean) / x_std
         y = self.PatchEncoder(x_normalised)
         z = self.NeuralSolver(y, t_final)