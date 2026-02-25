519e7181c840a155ab1a220741e1accd3717e042
Katie795 <katherinemackenzie795@gmail.com>
2026-02-25 22:05:42 +0000

    separate evaluate and plot

--- neural_pde/evaluate.py
+++ neural_pde/evaluate.py
@@ -1,22 +1,16 @@
-"""Evaluate a saved model on a the test dataset"""
+"""Evaluate a saved model on the test dataset"""
 
 import torch
 from torch.utils.data import DataLoader
 from firedrake import *
-import os
-import shutil
 import tomllib
 import argparse
 import numpy as np
-from neural_pde.util.diagnostics import Diagnostics
 from neural_pde.data.datasets import load_hdf5_dataset, show_hdf5_header
-from neural_pde.util.velocity_functions import Projector as Proj
 from neural_pde.util.loss_functions import (
     multivariate_normalised_rmse_with_data as metric,
 )
-from neural_pde.util.loss_functions import individual_function_rmse as metric2
 from neural_pde.model.model import load_model
-import matplotlib.pyplot as plt
 
 # Create argparse arguments
 parser = argparse.ArgumentParser()
@@ -35,32 +29,6 @@ parser.add_argument(
     action="store",
     help="whether to use the training, testing, or validation dataset",
     default="test",
-)
-
-parser.add_argument(
-    "--animate",
-    action="store_true",
-    help="whether to produce an animation using the neural network model",
-)
-
-parser.add_argument(
-    "--plot_dataset_and_model",
-    action="store_true",
-    help="whether to produce vtu files for the dataset",
-)
-
-parser.add_argument(
-    "--animate_dataset",
-    action="store_true",
-    help="whether to animate the given dataset (default test dataset)",
-)
-
-parser.add_argument(
-    "--output",
-    type=str,
-    action="store",
-    help="path to output folder",
-    default="../results/output_for_evaluation",
 )
 
 parser.add_argument(
@@ -97,10 +65,13 @@ dataset = load_hdf5_dataset(f"{args.data_directory}{config["data"][args.dataset]
 
 batch_size = config["optimiser"]["batchsize"]
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
 train_ds = load_hdf5_dataset(f"{args.data_directory}{config["data"]["train"]}")
+
 overall_mean = torch.mean(torch.from_numpy(train_ds.mean), axis=1)[
     train_ds.n_func_in_dynamic + train_ds.n_func_in_ancillary :
 ].to(device)
+
 overall_std = torch.mean(torch.from_numpy(train_ds.std), axis=1)[
     train_ds.n_func_in_dynamic + train_ds.n_func_in_ancillary :
 ].to(device)
@@ -120,152 +91,3 @@ for (Xv, tv), yv in dataloader:
     avg_loss += loss.item() / (dataset.n_samples / batch_size)
 
 print(f"average relative error: {100 * avg_loss:6.3f} %")
-
-if not os.path.exists(args.output):
-    os.makedirs(args.output)
-
-mesh = IcosahedralSphereMesh(dataset.radius, dataset.n_ref)
-V = FunctionSpace(mesh, "CG", 1)
-V_DG = FunctionSpace(mesh, "DG", 0)
-(X, _), __ = next(iter(dataset))
-dt = config["architecture"]["dt"] / 26349.050394344416
-t_initial = dataset._t_initial 
-t_final = dataset._t_final 
-animation_file_nn = VTKFile(os.path.join(args.output, "animation.pvd"))
-h_pred = Function(V, name="h")
-div_pred = Function(V, name="div")
-vor_pred = Function(V, name="vor")
-
-# for the animation, we don't need any testing data, only the inital state!
-with CheckpointFile("results/gusto_output/chkpt.h5", "r") as afile:
-
-    mesh_h5 = afile.load_mesh("IcosahedralMesh")
-    x, y, z = SpatialCoordinate(mesh_h5)  # spatial coordinate
-    V_DG = FunctionSpace(mesh_h5, "DG", 1)
-    V_CG = FunctionSpace(mesh_h5, "CG", 1)
-    V_BDM = FunctionSpace(mesh_h5, "BDM", 2)
-    x_fun = Function(V_CG).interpolate(x)  # collect data on x,y,z coordinates
-    y_fun = Function(V_CG).interpolate(y)
-    z_fun = Function(V_CG).interpolate(z)
-
-    h_inp = Function(V_CG)  # input function for h
-    w1 = afile.load_function(mesh_h5, "u", idx=0)
-    h1 = afile.load_function(mesh_h5, "D", idx=0)
-    p2 = Proj(V_DG, V_CG)
-    p2.apply(h1, h_inp)
-    diagnostics = Diagnostics(V_BDM, V_CG)
-    vorticity_inp = diagnostics.vorticity(w1)
-    divergence_inp = diagnostics.divergence(w1)
-    X = np.zeros((1, 6, mesh_h5.num_vertices()), dtype=np.float64)
-    # input data
-    X[0, 0, :] = h_inp.dat.data  # h data
-    X[0, 1, :] = divergence_inp.dat.data
-    X[0, 2, :] = vorticity_inp.dat.data
-
-    X[0, 3, :] = x_fun.dat.data  # x coord data
-    X[0, 4, :] = y_fun.dat.data  # y coord data
-    X[0, 5, :] = z_fun.dat.data  # z coord data
-    X = torch.tensor(X, dtype=torch.float32)
-
-if args.animate_dataset:
-    sorted_t, indices = torch.sort(torch.tensor(dataset._t_initial))
-    sorted_X = torch.tensor(dataset._data[indices, :, :])
-
-    for i in range(len(sorted_t)):
-        f_input_d = Function(V, name=f"height")
-        f_input_d.dat.data[:] = sorted_X.detach().cpu().numpy()[i, 0, :]
-        f_input_div = Function(V, name="divergence")
-        f_input_div.dat.data[:] = sorted_X.detach().cpu().numpy()[i, 1, :]
-        f_input_vor = Function(V, name="vorticity")
-        f_input_vor.dat.data[:] = sorted_X.detach().cpu().numpy()[i, 2, :]
-
-        file = VTKFile(
-                os.path.join(args.output, f"dataset_animation/order_{i}.pvd")
-            )
-        file.write(
-            f_input_d,
-            f_input_div,
-            f_input_vor,
-        )
-    
-    for item in os.listdir(f"{args.output}/dataset_animation"):
-        if item.endswith(".pvd"):
-            pass
-        else:
-            subfolder = os.path.join(f"{args.output}/dataset_animation", item)
-            for subitem in os.listdir(subfolder):
-                subitem
-                itempath = os.path.join(subfolder, subitem)
-                itemnewpath = os.path.join(f"{args.output}/dataset_animation", subitem)
-                shutil.move(itempath, itemnewpath)
-                itemnewname = os.path.join(f"{args.output}/dataset_animation", subitem[:-6] + subitem[-4:])
-                os.rename(itemnewpath, itemnewname)
-    
-
-
-if args.animate:
-    t = 0
-    while t < 20:
-        y_pred = model(X, torch.tensor(t))
-
-        h_pred.dat.data[:] = y_pred.detach().cpu().numpy()[0, 0, :]
-        div_pred.dat.data[:] = y_pred.detach().cpu().numpy()[0, 1, :]
-        vor_pred.dat.data[:] = y_pred.detach().cpu().numpy()[0, 2, :]
-        animation_file_nn.write(h_pred, div_pred, vor_pred, time=t)
-
-        t += dt
-
-        print(f"time = {t:8.4f}")
-
-
-if args.plot_dataset_and_model:
-    print("Plotting dataset and model")
-    fig, ax = plt.subplots()
-    for j, ((X, t), y_target) in enumerate(iter(dataset)):
-        X = X.to(device)
-        t = t.to(device)
-        y_target = y_target.to(device)
-        X = torch.unsqueeze(X, 0)
-        y_pred = model(X, t)
-        y_pred = torch.squeeze(y_pred, 0)
-        yt = torch.unsqueeze(y_target, 0)
-        print(f"Individual errors are {metric2(y_pred, yt, overall_mean, overall_std)}")
-        X = torch.squeeze(X, 0)
-
-        f_input_d = Function(V, name=f"input_d")
-        f_input_d.dat.data[:] = X.detach().cpu().numpy()[0, :]
-        f_input_div = Function(V, name="input_div")
-        f_input_div.dat.data[:] = X.detach().cpu().numpy()[1, :]
-        f_input_vor = Function(V, name="input_vor")
-        f_input_vor.dat.data[:] = X.detach().cpu().numpy()[2, :]
-
-        f_target_d = Function(V, name=f"target_d_t={t:6.3f}")
-        f_target_d.dat.data[:] = y_target.cpu().detach().numpy()[0, :]
-        f_target_div = Function(V, name="target_div")
-        f_target_div.dat.data[:] = y_target.cpu().detach().numpy()[1, :]
-        f_target_vor = Function(V, name="target_vor")
-        f_target_vor.dat.data[:] = y_target.cpu().detach().numpy()[2, :]
-        f_target = y_target.detach()[0:2, :]
-
-        f_pred_d = Function(V, name=f"pred_d_t={t:6.3f}")
-        f_pred_d.dat.data[:] = y_pred.detach().cpu().numpy()[0, :]
-        f_pred_div = Function(V, name="pred_div")
-        f_pred_div.dat.data[:] = y_pred.detach().cpu().numpy()[1, :]
-        f_pred_vor = Function(V, name="pred_vor")
-        f_pred_vor.dat.data[:] = y_pred.detach().cpu().numpy()[2, :]
-        f_pred = y_pred.detach()[0:2, :]
-
-        file = VTKFile(
-            os.path.join(args.output, f"dataset/output_t={t_initial[j]}_{j:04d}.pvd")
-        )
-        file.write(
-            f_input_d,
-            f_input_div,
-            f_input_vor,
-            f_target_d,
-            f_target_div,
-            f_target_vor,
-            f_pred_d,
-            f_pred_div,
-            f_pred_vor,
-        )
